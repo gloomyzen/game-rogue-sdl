c7724c72520ef7fd5a99085610725c8958d57109
Artur Barilo <faustkeller45@gmail.com>
2020-04-26 04:49:56 +0300

    master | game loop and try add emscripten

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,1 +1,58 @@
 #include "Game.hpp"
+
+Game::Game() {}
+
+Game::~Game() {}
+
+void Game::init(const char *title, int xpos, int ypos, int width, int height, bool fullscreen) {
+
+    int fullscreen_flag = fullscreen ? SDL_WINDOW_FULLSCREEN : 0;
+    Uint32 render_flag = SDL_RENDERER_ACCELERATED;
+
+    if(SDL_Init(SDL_INIT_EVERYTHING) == 0)
+    {
+        std::cout << "Initialize." << std::endl;
+        window = SDL_CreateWindow(title, xpos, ypos, width, height, fullscreen_flag);
+        if (window) {
+            std::cout << "Window created!" << std::endl;
+        }
+
+        renderer = SDL_CreateRenderer(window, -1, render_flag);
+        if (renderer)
+        {
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            std::cout << "Renderer created!" << std::endl;
+        }
+        isRunning = true;
+    } else {
+        isRunning = false;
+    }
+}
+
+void Game::handleEvents() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch (event.type) {
+        case SDL_QUIT:
+            isRunning = false;
+            break;
+
+        default:
+            std::cout << "Unhandled event: " << event.type << std::endl;
+            break;
+    }
+}
+
+void Game::update() {}
+
+void Game::render() {
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
+
+void Game::clean() {
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+    std::cout << "Game stopped." << std::endl;
+}
--- src/Game.hpp
+++ src/Game.hpp
@@ -4,6 +4,10 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
+#ifdef EMSCRIPTEN_IS_USED
+#include <emscripten.h>
+#endif //EMSCRIPTEN_IS_USED
+
 class Game {
 
 public:
@@ -11,9 +15,17 @@ public:
     ~Game();
 
     void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+    void handleEvents();
     void update();
     void render();
+    void clean();
+    bool running() { return isRunning; }
+
 private:
+    bool isRunning;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
 };
 
 
