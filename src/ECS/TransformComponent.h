ffdfb1e3a99edb5a60574fe3b6a8a27246c250f7
Artur Barilo <faustkeller45@gmail.com>
2020-05-03 19:07:28 +0300

    master | final movement feature

--- src/ECS/TransformComponent.h
+++ src/ECS/TransformComponent.h
@@ -46,49 +46,8 @@ public:
 
         deltaTime = (double)((NOW - LAST)*1000 / (double)SDL_GetPerformanceFrequency() );
 
-        if (std::abs(velocity.x) > 0 && velocity.x > 0 && movement.x >= 0 || velocity.x < 0 && movement.x <= 0) {
-            movement.x = std::abs(movement.x + (deltaSpeed / deltaTime)) <= maxSpeed ? movement.x + (deltaSpeed / deltaTime) * velocity.x : maxSpeed * velocity.x;
-        } else {
-            if (movement.x > 0) {
-                std::cout << "one" << std::endl;
-                movement.x = movement.x - (deltaSpeed / deltaTime * 2);
-                movement.x = std::abs(movement.x) > 0.1f ? movement.x : 0;
-            } else if (movement.x < 0) {
-                std::cout << "two" << std::endl;
-                movement.x = movement.x + (deltaSpeed / deltaTime * 2);
-                movement.x = std::abs(movement.x) > 0.1f ? movement.x : 0;
-            } else {
-                movement.x = 0;
-            }
-        }
-        std::cout << movement.x << std::endl;
-//        if (std::abs(movement.x + (deltaSpeed / deltaTime)) <= maxSpeed) {
-//            movement.x = movement.x + (deltaSpeed / deltaTime) * velocity.x;
-//        } else {
-//            movement.x = maxSpeed * velocity.x;
-//        }
-//        movement.x = std::abs(movement.x + (deltaSpeed / deltaTime)) <= maxSpeed ? movement.x + (deltaSpeed / deltaTime) * velocity.x : maxSpeed * velocity.x;
-
-
-        if (std::abs(velocity.x) > 0) {
-//            movement.x = std::abs(movement.x + (deltaSpeed / deltaTime)) <= maxSpeed ? (movement.x + (deltaSpeed / deltaTime)) * velocity.x : maxSpeed * velocity.x;
-        } else {
-//            movement.x = std::abs(movement.x - (deltaSpeed / deltaTime * 2)) < deltaSpeed ? movement.x - (deltaSpeed / deltaTime * 2) : 0;
-        }
-//        std::cout << movement.x << std::endl;
-
-//        if (velocity.y != 0) {
-//            movement.y = std::abs(movement.y + (deltaSpeed / deltaTime)) <= maxSpeed ? movement.y + (deltaSpeed / deltaTime) * velocity.y : maxSpeed * velocity.y;
-//        } else {
-//            if (movement.y > 0) {
-//                movement.y = movement.y > 0 ? movement.y - (deltaSpeed / deltaTime * 2) : 0;
-//            } else if (movement.y < 0) {
-//                movement.y = movement.y < 0 ? movement.y + (deltaSpeed / deltaTime * 2) : 0;
-//            }
-//        }
-
-        position.x += movement.x;
-        position.y += movement.y;
+        position.x += movement.x = getMovement(&velocity.x, &movement.x, &deltaTime);
+        position.y += movement.y = getMovement(&velocity.y, &movement.y, &deltaTime);
     }
 
     Vector2D getPosition()
@@ -100,5 +59,27 @@ public:
     {
         position.x = x;
         position.y = y;
+    }
+
+private:
+    float getMovement(const float *pVelocity, float *pMovement, const double *pDeltaTime) {
+        if (std::abs(*pVelocity) > 0 && ((*pVelocity > 0 && *pMovement >= 0) || (*pVelocity < 0 && *pMovement <= 0))) {
+            if (std::abs(*pMovement + (deltaSpeed / *pDeltaTime)) <= maxSpeed) {
+                *pMovement = *pMovement + (deltaSpeed / *pDeltaTime) * *pVelocity;
+            } else {
+                *pMovement = maxSpeed * *pVelocity;
+            }
+        } else {
+            if (*pMovement > 0) {
+                *pMovement = *pMovement - (deltaSpeed / *pDeltaTime * 2);
+                *pMovement = std::abs(*pMovement) > 0.1f ? *pMovement : 0;
+            } else if (*pMovement < 0) {
+                *pMovement = *pMovement + (deltaSpeed / *pDeltaTime * 2);
+                *pMovement = std::abs(*pMovement) > 0.1f ? *pMovement : 0;
+            } else {
+                *pMovement = 0;
+            }
+        }
+        return *pMovement;
     }
 };
