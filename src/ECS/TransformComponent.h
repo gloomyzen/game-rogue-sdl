e169f037acb71d8834059e62b4bfd36e5eeddc9c
Artur Barilo <faustkeller45@gmail.com>
2020-05-03 18:37:56 +0300

    master | add KeyboardController.h, in process

--- src/ECS/TransformComponent.h
+++ src/ECS/TransformComponent.h
@@ -12,9 +12,14 @@ public:
 
     Vector2D position;
     Vector2D velocity;
+    Vector2D movement;
+//
+    Uint64 NOW = SDL_GetPerformanceCounter();
+    Uint64 LAST = 0;
+    double deltaTime = 0;
 
-    int defaultSpeed = 1;
-    int speed = 1;
+    const float deltaSpeed = 1.0f;
+    const int maxSpeed = 3;
 
     TransformComponent()
     {
@@ -30,14 +35,60 @@ public:
 
     void init() override
     {
-        velocity.x = 0;
-        velocity.y = 0;
+//        velocity.x = 0;
+//        velocity.y = 0;
     }
 
     void update() override
     {
-        position.x += velocity.x * (float)speed;
-        position.y += velocity.y * (float)speed;
+        LAST = NOW;
+        NOW = SDL_GetPerformanceCounter();
+
+        deltaTime = (double)((NOW - LAST)*1000 / (double)SDL_GetPerformanceFrequency() );
+
+        if (std::abs(velocity.x) > 0 && velocity.x > 0 && movement.x >= 0 || velocity.x < 0 && movement.x <= 0) {
+            movement.x = std::abs(movement.x + (deltaSpeed / deltaTime)) <= maxSpeed ? movement.x + (deltaSpeed / deltaTime) * velocity.x : maxSpeed * velocity.x;
+        } else {
+            if (movement.x > 0) {
+                std::cout << "one" << std::endl;
+                movement.x = movement.x - (deltaSpeed / deltaTime * 2);
+                movement.x = std::abs(movement.x) > 0.1f ? movement.x : 0;
+            } else if (movement.x < 0) {
+                std::cout << "two" << std::endl;
+                movement.x = movement.x + (deltaSpeed / deltaTime * 2);
+                movement.x = std::abs(movement.x) > 0.1f ? movement.x : 0;
+            } else {
+                movement.x = 0;
+            }
+        }
+        std::cout << movement.x << std::endl;
+//        if (std::abs(movement.x + (deltaSpeed / deltaTime)) <= maxSpeed) {
+//            movement.x = movement.x + (deltaSpeed / deltaTime) * velocity.x;
+//        } else {
+//            movement.x = maxSpeed * velocity.x;
+//        }
+//        movement.x = std::abs(movement.x + (deltaSpeed / deltaTime)) <= maxSpeed ? movement.x + (deltaSpeed / deltaTime) * velocity.x : maxSpeed * velocity.x;
+
+
+        if (std::abs(velocity.x) > 0) {
+//            movement.x = std::abs(movement.x + (deltaSpeed / deltaTime)) <= maxSpeed ? (movement.x + (deltaSpeed / deltaTime)) * velocity.x : maxSpeed * velocity.x;
+        } else {
+//            movement.x = std::abs(movement.x - (deltaSpeed / deltaTime * 2)) < deltaSpeed ? movement.x - (deltaSpeed / deltaTime * 2) : 0;
+        }
+//        std::cout << movement.x << std::endl;
+
+//        if (velocity.y != 0) {
+//            movement.y = std::abs(movement.y + (deltaSpeed / deltaTime)) <= maxSpeed ? movement.y + (deltaSpeed / deltaTime) * velocity.y : maxSpeed * velocity.y;
+//        } else {
+//            if (movement.y > 0) {
+//                movement.y = movement.y > 0 ? movement.y - (deltaSpeed / deltaTime * 2) : 0;
+//            } else if (movement.y < 0) {
+//                movement.y = movement.y < 0 ? movement.y + (deltaSpeed / deltaTime * 2) : 0;
+//            }
+//        }
+
+        position.x += movement.x;
+        position.y += movement.y;
     }
 
     Vector2D getPosition()
