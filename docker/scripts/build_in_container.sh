dd5b36f03703e541c13a0173de2c1434039c0029
Artur Barilo <faustkeller45@gmail.com>
2020-05-06 05:55:47 +0300

    master | update build flags

--- docker/scripts/build_in_container.sh
+++ docker/scripts/build_in_container.sh
@@ -5,4 +5,4 @@
 PROJECT_PATH=/var/www
 BUILD_FOLDER=public
 
-cd ${PROJECT_PATH} && rm -rf ${BUILD_FOLDER} && mkdir ${BUILD_FOLDER} && cd ${BUILD_FOLDER} && source "/var/emsdk/emsdk_env.sh" && cmake -DBUILD_TYPE=webassembly .. && make -j$(nproc)
+cd ${PROJECT_PATH} && rm -rf ${BUILD_FOLDER} && mkdir ${BUILD_FOLDER} && cd ${BUILD_FOLDER} && source "/var/emsdk/emsdk_env.sh" && cmake -DBUILD_TYPE=webassembly -DCMAKE_BUILD_TYPE=Debug .. && make -j$(nproc)
