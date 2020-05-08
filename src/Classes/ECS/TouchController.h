2cd81901a6a3a62f9ff3bcc2e67a637b79db433f
Artur Barilo <faustkeller45@gmail.com>
2020-05-08 19:33:07 +0300

    master | minor refactoring

--- src/Classes/ECS/TouchController.h
+++ src/Classes/ECS/TouchController.h
@@ -15,7 +15,6 @@ public:
     float touchY;
     float motionX;
     float motionY;
-    bool isActive;
 
     TouchController() = default;
 
@@ -36,46 +35,41 @@ public:
         //TODO need try to use Game::event.tfinger.dx|dy
         // event.tfinger.dx|dy contain the distance moved in the x-axis, normalized (-1...1)
         if (Game::event.type == SDL_FINGERDOWN) {
-            isActive = true;
             touchX = Game::event.tfinger.x;
             touchY = Game::event.tfinger.y;
         }
         if (Game::event.type == SDL_FINGERMOTION) {
             motionX = Game::event.tfinger.x;
             motionY = Game::event.tfinger.y;
-            if (isActive) {
-                if (std::abs(touchX - motionX) > 0.01f && touchX < motionX) {
-                    transform->velocity.x = 1;
-                    motionY = Game::event.tfinger.y;
-                    sprite->play("Walk");
+            if (std::abs(touchX - motionX) > 0.01f && touchX < motionX) {
+                transform->velocity.x = 1;
+                motionY = Game::event.tfinger.y;
+                sprite->play("Walk");
                 sprite->spriteFlip = SDL_FLIP_NONE;
-                } else if (std::abs(touchX - motionX) > 0.01f && touchX > motionX) {
-                    transform->velocity.x = -1;
-                    motionY = Game::event.tfinger.y;
-                    sprite->play("Walk");
-                    sprite->spriteFlip = SDL_FLIP_HORIZONTAL;
-                }
-                if (std::abs(touchY - motionY) > 0.01f && touchY < motionY) {
-                    transform->velocity.y = 1;
-                    motionX = Game::event.tfinger.x;
-                    sprite->play("Walk");
-                } else if (std::abs(touchY - motionY) > 0.01f && touchY > motionY) {
-                    transform->velocity.y = -1;
-                    motionX = Game::event.tfinger.x;
-                    sprite->play("Walk");
-                }
+            } else if (std::abs(touchX - motionX) > 0.01f && touchX > motionX) {
+                transform->velocity.x = -1;
+                motionY = Game::event.tfinger.y;
+                sprite->play("Walk");
+                sprite->spriteFlip = SDL_FLIP_HORIZONTAL;
+            } else {
+                transform->velocity.x = 0;
+            }
+            if (std::abs(touchY - motionY) > 0.01f && touchY < motionY) {
+                transform->velocity.y = 1;
+                motionX = Game::event.tfinger.x;
+                sprite->play("Walk");
+            } else if (std::abs(touchY - motionY) > 0.01f && touchY > motionY) {
+                transform->velocity.y = -1;
+                motionX = Game::event.tfinger.x;
+                sprite->play("Walk");
+            } else {
+                transform->velocity.y = 0;
             }
         }
         if (Game::event.type == SDL_FINGERUP) {
-            isActive = false;
             transform->velocity.x = 0;
             transform->velocity.y = 0;
             sprite->play("Idle");
-        }
-        if (!isActive) {
-//            transform->velocity.x = 0;
-//            transform->velocity.y = 0;
-//            sprite->play("Idle");
         }
     }
 };
