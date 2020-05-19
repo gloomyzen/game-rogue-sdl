426179a96bca6531779cfb916c323fae956d47e8
Artur Barilo <faustkeller45@gmail.com>
2020-05-19 15:04:42 +0900

    master [skip ci] FileLoadManager in process

--- src/Classes/Utilities/LoadManager.h
+++ src/Classes/Utilities/LoadManager.h
@@ -4,22 +4,29 @@
 #define ROGUE_SDL_LOADMANAGER_H
 
 #include "JsonLoadManager.h"
+#include "../Scenes/Scenes.h"
+#include "../GameObjects/GameObject.h"
+#include <map>
 
 class LoadManager {
 private:
     bool mLoaded;
     JsonLoadManager* pJsonManager;
+    std::map<std::string, Scene> mScenes;
+    std::map<std::string, GameObject> mGameObjects;
 
     void Parse() {
-        //
+        //TODO recursive for loop for get all scenes and game objects
     }
+
+
 
 public:
     explicit LoadManager(const char* &filePath) {
         mLoaded = false;
         pJsonManager->loadFile(filePath);
         if (pJsonManager->isLoaded()) {
-//            Parse();
+            Parse();
         }
     }
 
